19d4792d0b6e91616ea0ee26f1be4be9e4cbb16b
Yaroslav Svitlytskyi <yariksvitlitskiy81@gmail.com>
2024-01-04 23:23:21 +0100

    fix: fixed bugs

--- script.sh
+++ script.sh
@@ -2,11 +2,12 @@
 
 # Constants, which represent pre-defined regexp to analyse
 # C code.
-readonly FUNCTION_REGEXP_DECL_KEY='^[a-z]+ ([a-z]+)\(\) \{$'
+readonly FUNCTION_DECLARATION_REGEXP_DECL_KEY='^[a-z]+ ([a-z]+)\(\)( )?\{$'
+readonly FUNCTION_CALL_REGEXP_DECL_KEY='^([a-zA-Z]+)\(\)$'
 readonly RETURN_REGEXP_DECL_KEY=''
-readonly PRINT_REGEXP_DECL_KEY=''
-readonly INCLUDE_REGEXP_DECL_KEY='(^#include <[a-z]+(\.h)?>)|(#include "[a-z]+(\.h)?")$'
-readonly FOR_CYCLE_REGEXP_DECL_KEY='^for \([a-z]+ [a-z]+ = [0-9]+\; [a-z]+ \< [0-9]+\; [a-z]+\+\+\) \{$'
+readonly PRINT_REGEXP_DECL_KEY='^printf\("([a-zA-Z0-9 ]+)"\)\;$'
+readonly INCLUDE_REGEXP_DECL_KEY='(^#include( )?<[a-z]+(\.h)?>)|(#include "[a-z]+(\.h)?")$'
+readonly FOR_CYCLE_REGEXP_DECL_KEY='^for( )?\([a-z]+ [a-z]+( )?=( )?[0-9]+\;( )?[a-z]+( )?\<( )?[0-9]+\;( )?[a-z]+\+\+\)( )?\{$'
 
 
 # readonly INTEGER_TYPE_DECL_KEY='int'
@@ -22,24 +23,27 @@ readonly DO_RESERVED_KEY='do'
 readonly RETURN_RESERVED_KEY='return'
 readonly BREAK_RESERVED_KEY='break'
 readonly CONTINUE_RESERVED_KEY='continue'
-readonly VOID_RESERVED_KEY='void'
 readonly FUNCTION_RESERVED_KEY='function'
 readonly RIGHT_BRACKET_RESERVED_KEY='}'
 readonly LEFT_BRACKET_RESERVED_KEY='{'
+readonly RIGHT_CURVED_BRACKET_RESERVED_KEY=')'
+readonly LEFT_CURVED_BRACKET_RESERVED_KEY='('
+readonly RIGHT_CUBIC_BRACKET_RESERVED_KEY=']'
+readonly LEFT_CUBIC_BRACKET_RESERVED_KEY='['
+
+readonly ENTRYPOINT_FUNCTION_RESERVED_KEY='main'
 
 # Describes storage related properties.
 readonly LOCK_STORAGE_DECL_KEY='lock_storage'
 readonly CYCLE_LOCK_STORAGE_DECL_KEY='cycle'
+
+readonly FUNCTIONS_STORAGE_DECL_KEY='functions_storage'
 
 readonly SCOPE_STORAGE_DECL_KEY='scope'
 readonly INDEX_SCOPE_STORAGE_DECL_KEY='index'
 
 readonly REGEXP_MATCH_STORAGE_DECL_KEY='regexp_storage'
 readonly LAST_REGEXP_MATCH_DECL_KEY='last'
-
-# # Describes pointers for the output file.
-# PREVIOUS_OUTPUT_POINTER=0
-# CURRENT_OUTPUT_POINTER=0
 
 # Describes amount of spaces, which should be applied
 # for each scope index number increase.
@@ -60,6 +64,16 @@ function map_keys() {
     alias -p | grep $1 | cut -d'=' -f1 | awk -F"$1" '{print $2; }'
 }
 
+# Checks if the given key exists in a global map.
+function map_exists() {
+    for key in $(map_keys $FUNCTIONS_STORAGE_DECL_KEY); do
+        if [[ $(map_get $FUNCTIONS_STORAGE_DECL_KEY $key) == $1 ]]; then
+            return 0
+        fi
+    done
+    return 1
+}
+ 
 function set_last_regexp_match() {
     map_put $REGEXP_MATCH_STORAGE_DECL_KEY $LAST_REGEXP_MATCH_DECL_KEY $1
 }
@@ -144,9 +158,35 @@ function is_ignorable() {
 
 # Checks if the given line is a function beginning declaration.
 function is_function_beginning() {
-    if [[ $1 =~ $FUNCTION_REGEXP_DECL_KEY ]]; then
+    if [[ $1 =~ $FUNCTION_DECLARATION_REGEXP_DECL_KEY ]]; then
         set_last_regexp_match "${BASH_REMATCH[1]}"
-        # map_put $REGEXP_MATCH_STORAGE_DECL_KEY $LAST_REGEXP_MATCH_DECL_KEY "${BASH_REMATCH[1]}"
+        return 0
+    else    
+        return 1
+    fi
+}
+
+# Checks if the given line is a function call declaration.
+function is_function_call() {
+    if [[ $1 =~ $FUNCTION_CALL_REGEXP_DECL_KEY ]]; then
+        set_last_regexp_match "${BASH_REMATCH[1]}"
+        return 0
+    else    
+        return 1
+    fi
+}
+
+function is_ending_bracket() {
+    if [[ $1 =~ $RIGHT_BRACKET_RESERVED_KEY ]]; then
+        return 0
+    else    
+        return 1
+    fi
+}
+
+function is_printf() {
+    if [[ $1 =~ $PRINT_REGEXP_DECL_KEY ]]; then
+        set_last_regexp_match "${BASH_REMATCH[1]}"
         return 0
     else    
         return 1
@@ -156,6 +196,23 @@ function is_function_beginning() {
 # Checks if the given line is a for cycle declaration.
 function is_for_cycle() {
     if [[ $1 =~ $FOR_CYCLE_REGEXP_DECL_KEY ]]; then
+        set_last_regexp_match "${BASH_REMATCH[1]}"
+        return 0
+    else    
+        return 1
+    fi
+}
+
+function is_break() {
+    if [[ $1 =~ $BREAK_RESERVED_KEY ]]; then
+        return 0
+    else    
+        return 1
+    fi
+}
+
+function is_continue() {
+    if [[ $1 =~ $CONTINUE_RESERVED_KEY ]]; then
         return 0
     else    
         return 1
@@ -180,7 +237,7 @@ function is_for_cycle() {
 # 
 function retrieve_shift() {
     local shift=""
-    for ((i=0;i<retrieve_index_scope;i++)); do
+    for ((i=0;i<$(retrieve_index_scope);i++)); do
         for ((g=1;g<=SCOPE_INDEX_SHIFT;g++)); do
             shift+=" "
         done
@@ -191,11 +248,36 @@ function retrieve_shift() {
 
 
 function compose_function_beginning() {
-    echo "$(retrieve_shift)$FUNCTION_RESERVED_KEY $1() $LEFT_BRACKET_RESERVED_KEY"
+    echo "$(retrieve_shift)$FUNCTION_RESERVED_KEY $1$LEFT_CURVED_BRACKET_RESERVED_KEY$RIGHT_CURVED_BRACKET_RESERVED_KEY $LEFT_BRACKET_RESERVED_KEY"
 }
 
-function compose_function_ending() {
-    echo "$(retrieve_shift)${$RIGHT_BRACKET_RESERVED_KEY}"
+function compose_function_call() {
+    echo "$(retrieve_shift)$ $1$LEFT_CURVED_BRACKET_RESERVED_KEY$RIGHT_CURVED_BRACKET_RESERVED_KEY"
+}
+
+function compose_for_cycle_beginning() {
+    echo "$(retrieve_shift)$FOR_RESERVED_KEY $LEFT_CURVED_BRACKET_RESERVED_KEY$LEFT_CURVED_BRACKET_RESERVED_KEY $RIGHT_CURVED_BRACKET_RESERVED_KEY$RIGHT_CURVED_BRACKET_RESERVED_KEY $LEFT_BRACKET_RESERVED_KEY"
+}
+
+function compose_break() {
+    echo "$(retrieve_shift)$BREAK_RESERVED_KEY"
+}
+
+function compose_continue() {
+    echo "$(retrieve_shift)$CONTINUE_RESERVED_KEY"
+}
+
+function compose_printf() {
+    echo "$(retrieve_shift)echo \"$1\""
+}
+
+function compose_ending_bracket() {
+    echo "$(retrieve_shift)$RIGHT_BRACKET_RESERVED_KEY"
+}
+
+function compose_entrypoint_execution() {
+    printf "\n"
+    echo "$ENTRYPOINT_FUNCTION_RESERVED_KEY$LEFT_CURVED_BRACKET_RESERVED_KEY$RIGHT_CURVED_BRACKET_RESERVED_KEY"
 }
 
 
@@ -228,9 +310,42 @@ function main() {
             continue
         fi
 
+        is_function_call "$line"
+        if [[ $? == 0 ]]; then
+            write_to_output "$(compose_function_call $(retrieve_last_regexp_match))" $2
+            continue
+        fi
+
         is_for_cycle "$line"
         if [[ $? == 0 ]]; then
+            write_to_output "$(compose_for_cycle_beginning)" $2
             increase_index_scope
+            continue
+        fi
+
+        is_break "$line"
+        if [[ $? == 0 ]]; then
+            write_to_output "$(compose_break)" $2
+            continue
+        fi
+
+        is_continue "$line"
+        if [[ $? == 0 ]]; then
+            write_to_output "$(compose_continue)" $2
+            continue
+        fi
+
+        is_printf "$line"
+        if [[ $? == 0 ]]; then
+            retrieve_last_regexp_match
+            write_to_output "$(compose_printf $(retrieve_last_regexp_match))" $2
+            continue
+        fi
+
+        is_ending_bracket "$line"
+        if [[ $? == 0 ]]; then
+            decrease_index_scope
+            write_to_output "$(compose_ending_bracket)" $2
             continue
         fi
 
@@ -239,6 +354,10 @@ function main() {
         echo "$line"
     done < $1
 
+    write_to_output "$(compose_entrypoint_execution)" $2
+
+    map_exists $1
+    echo $?
     # echo $(map_keys $LOCK_STORAGE_DECL_KEY)
 }
 
